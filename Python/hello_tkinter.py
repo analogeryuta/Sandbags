00596abfe3095b30d05942b009b216452d48c215
Yuta AKAMINE <analogeryuta1984@gmail.com>
2010-09-29 08:26:42 +0900

    add new python script samples.

---
+++ Python/hello_tkinter.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python
+
+import Tkinter as Tk
+
+la = Tk.Label(None, text='Hello World!', font=('Times','18'))
+la.pack()
+la.mainloop()
---
+++ Python/hello_tkinter_py3.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python3
+
+import tkinter as Tk
+
+la = Tk.Label(None, text='Hello World!', font=('Times','18'))
+la.pack()
+la.mainloop()

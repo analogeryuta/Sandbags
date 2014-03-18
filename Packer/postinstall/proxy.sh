a398deba95497f768f9f3600b11121c09c09311e
Yuta, AKAMINE <y-akamine1984@occ.co.jp>
2014-03-18 18:08:16 +0900

    change several files.

---
+++ Packer/postinstall/proxy.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+# Proxy settings
+
+proxy='http://proxy.example.co.jp:8080'
+
+# wget
+echo "http_proxy = $proxy"  >> /etc/wgetrc
+echo "https_proxy = $proxy" >> /etc/wgetrc
+
+# curl
+echo "proxy = $proxy" >> /etc/curlrc
+
+# System environement variable
+echo "export http_proxy=$proxy"  >> /etc/environment
+echo "export https_proxy=$proxy" >> /etc/environment
+
+if [ -f /etc/system-release ]; then
+  echo "proxy=$proxy" >> /etc/yum.conf
+fi
+
+if [ -f /etc/debian_version ]; then
+  echo "Acquire::http::proxy \"$proxy\";"  >> /etc/apt/apt.conf.d/02proxy
+  echo "Acquire::https::proxy \"$proxy\";" >> /etc/apt/apt.conf.d/02proxy
+fi

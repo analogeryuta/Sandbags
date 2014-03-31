641db0315febbeebd0851aee2b2751f500d1d977
Yuta, AKAMINE <y-akamine1984@occ.co.jp>
2014-03-31 11:44:59 +0900

    modify small change.

--- Vagrant/debian-box/manifests/default.pp
+++ Vagrant/debian-box/manifests/default.pp
@@ -1,16 +1,3 @@
-### display message when user login.
-group { "puppet":
-  ensure => "present",
-}
-
-File { owner => 0, group => 0, mode => 0644 }
-
-file { '/etc/motd':
-  content => "Welcome to your Vagrant-built virtual machine!
-              Managed by Puppet.\n"
-}
-
-
 ### load path settings
 Exec {
   path => "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"

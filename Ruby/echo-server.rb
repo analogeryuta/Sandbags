7d45a53f658463a007230639afc0514f2807178f
Yuta, AKAMINE <y-akamine1984@occ.co.jp>
2014-04-03 19:20:02 +0900

    Create for testing, Capistrano/Vagrant deployment process.

--- Ruby/echo-server.rb
+++ Ruby/echo-server.rb
@@ -1,26 +1,42 @@
 require "socket"
 
-gs = TCPServer.open("localhost", 12345)
-socks = [gs]
+# open socket connection
+gs = TCPServer.open("127.0.0.1", 12345)
 
+# cast for Array object
+socks = [gs]
+printf("socks: %s\n", socks)
+
+# IPSocket.addr
+# ["AF_INET", port_no, domain_name, ip_addr] => [port_no, domain_name, ip_addr]
 addr = gs.addr
-addr.shift
+addr.shift 
 printf("echo server is on: %s\n", addr.join(":"))
 
 while true
+  #IO::select(). check socket connection from client.
   nsock = select(socks)
-  next if nsock == nil
+
+  #If there's no connection, go to next loop.
+  next if nsock == nil 
+
   for s in nsock[0]
-    if s == gs
+    # if get TCPServer object(socket connection is established)
+    if s == gs 
+      # push TCPServer object to Array object
       socks.push(s.accept)
       print(s, " is accepted\n")
     else 
+      # if get TCPSocket object(socket connection was established)
       if s.eof?
+        # if get escape sequence
         print(s, " is gone\n")
         s.close
         socks.delete(s)
       else 
+        # if get any data, write same data to socket.
         str = s.gets
+        printf("accept data from client: %s", str)
         s.write(str)
       end
     end

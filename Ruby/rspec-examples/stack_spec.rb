ce7a54c3093b1fb48a055e286a2f11b1beb050e6
Yuta, AKAMINE <y-akamine1984@occ.co.jp>
2014-04-23 19:09:51 +0900

    add spec file.

---
+++ Ruby/rspec-examples/stack_spec.rb
@@ -0,0 +1,11 @@
+#
+# example of stack implementation's test code.
+#
+describe SymbolStack do
+  context 'when stack is empty' do
+  end
+  context 'when stack is full' do
+  end
+  context 'when stack is not empty or full' do
+  end
+end
